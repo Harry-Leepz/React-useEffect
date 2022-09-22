bfdb8cd22220532af796e5155b002491e96c85e4
Harry Dhillon <hdhillon478@gmail.com>
2022-09-22 12:36:14 +0100

    Add useEffect hook to check local storage and update state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // useEffect is used to check local storage for the key
+  // and if found update the state
+  // this prevents users having to log after page refresh
+  useEffect(() => {
+    const logInStatus = localStorage.getItem("isLoggedIn");
+    if (logInStatus === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
+  // remove logged in key from local storage
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
